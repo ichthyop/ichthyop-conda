658570767473981bcf5b7f3cd98f177cbc3c7dd6
barriern <nicolas.barrier@ird.fr>
2021-03-23 07:56:52 +0100

    Updated clean linux

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-conda install maven openjdk netcdf4 xarray dask cartopy matplotlib ffmpeg sphinxcontrib-programoutput sphinxcontrib-bibtex ipython sphinx_rtd_theme spyder
+conda install -y maven openjdk netcdf4 xarray dask cartopy matplotlib ffmpeg sphinxcontrib-programoutput sphinxcontrib-bibtex ipython sphinx_rtd_theme spyder
