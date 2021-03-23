034a4c80cde26a232fc7d8994fbdc97920dc9c46
barriern <nicolas.barrier@ird.fr>
2021-03-23 07:27:22 +0100

    Adding spyder to install script

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-conda install maven openjdk netcdf4 xarray dask cartopy matplotlib ffmpeg sphinxcontrib-programoutput sphinxcontrib-bibtex ipython sphinx_rtd_theme
+conda install maven openjdk netcdf4 xarray dask cartopy matplotlib ffmpeg sphinxcontrib-programoutput sphinxcontrib-bibtex ipython sphinx_rtd_theme spyder
